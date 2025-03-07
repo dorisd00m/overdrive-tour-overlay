a66d93af35669a5b188bf4ea770444ef7fc6e58e
Alfonsus <axeolume@gmail.com>
2025-03-08 01:11:47 +0700

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/overdrive-tour-overlay', // Ganti dengan nama repo
   server: {
     port: 3535
   }
